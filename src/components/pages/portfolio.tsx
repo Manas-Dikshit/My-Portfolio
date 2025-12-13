dcbf4819e88f70e5cfb2f3f0b9753c4d098f040e
Manas-Dikshit <manasdikshit48@gmail.com>
2025-12-14 01:19:04 +0530

    jj

--- src/components/pages/portfolio.tsx
+++ src/components/pages/portfolio.tsx
@@ -5,7 +5,6 @@ import Navbar from "./sections/navbar";
 import Hero from "./sections/hero";
 import Projects from "./sections/projects";
 import About from "./sections/about";
-import Footer from "./sections/footer";
 import Contact from "./sections/contact";
 import Skills from "./sections/stats";
 import { Testimonials } from "./sections/testimonials";
