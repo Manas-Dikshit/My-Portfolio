b347d1081a36ec1ca80fb540325690d3ef607aa5
Manas-Dikshit <manasdikshit48@gmail.com>
2025-12-14 01:26:56 +0530

    DAtabase

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,7 +12,7 @@ const env = createEnv({
     // won't throw during module evaluation when env vars are missing.
     // In production you should provide the real values and tighten the schema.
     DATABASE_URL: z.string().refine(val => !val || val.startsWith('http://') || val.startsWith('https://') || val.startsWith('postgresql://'), {
-      message: 'DATABASE_URL must start with postgresql://, http://, or https://',
+      message: 'DATABASE_URL must start with postgresql://, http://,prisma+postgres://  or https://',
     }).optional(),
     NEXT_RUNTIME: z.enum(["nodejs", "edge"]).default("nodejs"),
     GITHUB_CLIENT_ID: z.string().optional(),
